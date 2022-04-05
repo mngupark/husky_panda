1db7d9c89583d1b4554ed6411333e0d0c4ecd461
Gyuhub <Gyuhub@github.com>
2022-04-05 18:05:48 +0900

    TODO: find a relationship between laser and fake_scan_link

--- husky_panda_mobile_slam_toolbox/src/husky_panda_mobile_scan.cpp
+++ husky_panda_mobile_slam_toolbox/src/husky_panda_mobile_scan.cpp
@@ -7,6 +7,7 @@
 #include "tf/transform_broadcaster.h"
 #include "tf/LinearMath/Quaternion.h"
 #include "tf2_ros/static_transform_broadcaster.h"
+#include "tf_conversions/tf_eigen.h"
 
 // messages
 #include "sensor_msgs/LaserScan.h"
@@ -15,22 +16,19 @@
 
 // pcl
 #include "laser_geometry/laser_geometry.h"
+#include "pcl/point_cloud.h"
+#include "pcl/point_types.h"
+#include "pcl/common/transforms.h"
+#include "pcl_conversions/pcl_conversions.h"
 
 class HuskyPandaMobileScanMerger
 {
 private:
-    float RAD2DEG(float rad) {
-        return 180.0 * rad / M_PI;
-    };
-    float DEG2RAD(float deg) {
-        return M_PI * deg / 180.0;
-    };
-    
     void front_scan_callback(const sensor_msgs::LaserScan::ConstPtr& scan);
     void rear_scan_callback(const sensor_msgs::LaserScan::ConstPtr& scan);
     void scan_merge();
-    void get_and_set_transform();
-    void convert_scan_to_cloud();
+    void get_transform();
+    void set_transform();
     void merge_clouds();
     void convert_cloud_to_scan();
 
@@ -61,13 +59,16 @@ private:
 
     // tf
     tf::StampedTransform front_scan_mount_transform_, rear_scan_mount_transform_, front_scan_transform_, rear_scan_transform_;
-    geometry_msgs::TransformStamped fake_scan_mount_transform, fake_scan_transform;
+    geometry_msgs::TransformStamped fake_scan_mount_transform_, fake_scan_transform_;
     // front scan transform
-    geometry_msgs::Vector3 front_scan_mount_transform_origin, rear_scan_mount_transform_origin, fake_scan_mount_transform_origin;
-    geometry_msgs::Quaternion fake_scan_mount_transform_rotation;
+    geometry_msgs::Vector3 front_scan_mount_transform_origin_, rear_scan_mount_transform_origin_, fake_scan_mount_transform_origin_;
+    geometry_msgs::Quaternion fake_scan_mount_transform_rotation_;
     // rear scan transform
-    geometry_msgs::Vector3 front_scan_transform_origin, rear_scan_transform_origin, fake_scan_transform_origin;
-    geometry_msgs::Quaternion fake_scan_transform_rotation;
+    geometry_msgs::Vector3 front_scan_transform_origin_, rear_scan_transform_origin_, fake_scan_transform_origin_;
+    geometry_msgs::Quaternion fake_scan_transform_rotation_;
+
+    // pcl
+    pcl::PointCloud<pcl::PointXYZ> front_pcl_cloud_, rear_pcl_cloud_;
 
 public:
     HuskyPandaMobileScanMerger(/* args */);
@@ -79,126 +80,182 @@ public:
 void HuskyPandaMobileScanMerger::front_scan_callback(const sensor_msgs::LaserScan::ConstPtr& scan) {
     front_scan_ = *scan;
     front_scan_projector_.transformLaserScanToPointCloud(front_scan_link_, front_scan_, front_cloud_, front_scan_listener_);
-    front_cloud_publisher_.publish(front_cloud_);
+    pcl::fromROSMsg(front_cloud_, front_pcl_cloud_);
+    // front_cloud_publisher_.publish(front_cloud_);
     return;
 }
 
 void HuskyPandaMobileScanMerger::rear_scan_callback(const sensor_msgs::LaserScan::ConstPtr& scan) {
     rear_scan_ = *scan;
+    rear_scan_projector_.transformLaserScanToPointCloud(rear_scan_link_, rear_scan_, rear_cloud_, rear_scan_listener_);
+    pcl::fromROSMsg(rear_cloud_, rear_pcl_cloud_);
+    rear_cloud_publisher_.publish(rear_cloud_);
     return;
 }
 
 void HuskyPandaMobileScanMerger::scan_merge() {
     while(nh_.ok()) {
-        get_and_set_transform();
-        convert_scan_to_cloud();
+        set_transform();    
         merge_clouds();
         convert_cloud_to_scan();
         ros::spinOnce();
     }
 }
 
-void HuskyPandaMobileScanMerger::get_and_set_transform() {
-        /// @todo get laser scan from two LiDARs and fusion them into a unique laser scan.
-        ///       Broadcast this scan at the middle pose of each scan.
-        try
-        {
-            front_scan_mount_listener_.lookupTransform(front_scan_mount_parent_link_, front_scan_mount_link_,
-                                     ros::Time::now(), front_scan_mount_transform_);
-        }
-        catch (tf::TransformException ex)
-        {
-            ROS_ERROR("%s", ex.what());
-            ros::Duration(1.0).sleep();
-        }
+void HuskyPandaMobileScanMerger::get_transform() {
+    try
+    {
+        // Verify that TF knows how to transform from the received scan to the destination scan frame
+        front_scan_mount_listener_.waitForTransform(front_scan_mount_parent_link_.c_str(), front_scan_mount_link_.c_str(), ros::Time::now(), ros::Duration(1));
+        front_scan_listener_.waitForTransform(front_scan_mount_link_.c_str(), front_scan_link_.c_str(), ros::Time::now(), ros::Duration(1));
+        rear_scan_mount_listener_.waitForTransform(rear_scan_mount_parent_link_.c_str(), rear_scan_mount_link_.c_str(), ros::Time::now(), ros::Duration(1));
+        rear_scan_listener_.waitForTransform(rear_scan_mount_link_.c_str(), rear_scan_link_.c_str(), ros::Time::now(), ros::Duration(1));
 
-        try
-        {
-            rear_scan_mount_listener_.lookupTransform(rear_scan_mount_parent_link_, rear_scan_mount_link_,
-                                     ros::Time::now(), rear_scan_mount_transform_);
-        }
-        catch (tf::TransformException ex)
-        {
-            ROS_ERROR("%s", ex.what());
-            ros::Duration(1.0).sleep();
-        }
+        front_scan_mount_listener_.lookupTransform(front_scan_mount_parent_link_, front_scan_mount_link_,
+                                                   ros::Time::now(), front_scan_mount_transform_);
+        rear_scan_mount_listener_.lookupTransform(rear_scan_mount_parent_link_, rear_scan_mount_link_,
+                                                  ros::Time::now(), rear_scan_mount_transform_);
+        front_scan_listener_.lookupTransform(front_scan_mount_link_, front_scan_link_,
+                                             ros::Time::now(), front_scan_transform_);
+        rear_scan_listener_.lookupTransform(rear_scan_mount_link_, rear_scan_link_,
+                                            ros::Time::now(), rear_scan_transform_);
+    }
+    catch (tf::TransformException ex)
+    {
+        ROS_ERROR("%s", ex.what());
+        ros::Duration(1.0).sleep();
+    }
+    /// @todo get laser scan from two LiDARs and fusion them into a unique laser scan.
+    ///       Broadcast this scan at the middle pose of each scan.
+    // // try
+    // // {
+    // //     front_scan_mount_listener_.lookupTransform(front_scan_mount_parent_link_, front_scan_mount_link_,
+    // //                              ros::Time::now(), front_scan_mount_transform_);
+    // // }
+    // // catch (tf::TransformException ex)
+    // // {
+    // //     ROS_ERROR("%s", ex.what());
+    // //     ros::Duration(1.0).sleep();
+    // // }
+    //
+    // // try
+    // // {
+    // //     rear_scan_mount_listener_.lookupTransform(rear_scan_mount_parent_link_, rear_scan_mount_link_,
+    // //                              ros::Time::now(), rear_scan_mount_transform_);
+    // // }
+    // // catch (tf::TransformException ex)
+    // // {
+    // //     ROS_ERROR("%s", ex.what());
+    // //     ros::Duration(1.0).sleep();
+    // // }
+    //
+    // // try
+    // // {
+    // //     front_scan_listener_.lookupTransform(front_scan_mount_link_, front_scan_link_,
+    // //                              ros::Time::now(), front_scan_transform_);
+    // // }
+    // // catch (tf::TransformException ex)
+    // // {
+    // //     ROS_ERROR("%s", ex.what());
+    // //     ros::Duration(1.0).sleep();
+    // // }
+    //
+    // // try
+    // // {
+    // //     rear_scan_listener_.lookupTransform(rear_scan_mount_link_, rear_scan_link_,
+    // //                              ros::Time::now(), rear_scan_transform_);
+    // // }
+    // // catch (tf::TransformException ex)
+    // // {
+    // //     ROS_ERROR("%s", ex.what());
+    // //     ros::Duration(1.0).sleep();
+    // // }
 
-        try
-        {
-            front_scan_listener_.lookupTransform(front_scan_mount_link_, front_scan_link_,
-                                     ros::Time::now(), front_scan_transform_);
-        }
-        catch (tf::TransformException ex)
-        {
-            ROS_ERROR("%s", ex.what());
-            ros::Duration(1.0).sleep();
-        }
-
-        try
-        {
-            rear_scan_listener_.lookupTransform(rear_scan_mount_link_, rear_scan_link_,
-                                     ros::Time::now(), rear_scan_transform_);
-        }
-        catch (tf::TransformException ex)
-        {
-            ROS_ERROR("%s", ex.what());
-            ros::Duration(1.0).sleep();
-        }
-
-        // tf::StampedTransform fake_scan_mount_transform, fake_scan_transform;
-        // fake_scan_mount_transform.frame_id_ = fake_scan_frame_id;
-        // fake_scan_mount_transform.child_frame_id_ = "fake_scan_mount_link";
-        // fake_scan_mount_transform.setOrigin((front_scan_mount_transform.getOrigin() + rear_scan_mount_transform.getOrigin()) / 2.0);
-        // fake_scan_mount_transform.setRotation(tf::Quaternion::getIdentity());
-        // fake_scan_mount_broadcaster.sendTransform(fake_scan_mount_transform);
-        //
-        // fake_scan_transform.frame_id_ = "fake_scan_mount_link";
-        // fake_scan_transform.child_frame_id_ = "fake_scan_link";
-        // fake_scan_transform.setOrigin((front_scan_transform.getOrigin() + rear_scan_transform.getOrigin()) / 2.0);
-        // fake_scan_transform.setRotation(tf::Quaternion::getIdentity());
-        // fake_scan_broadcaster.sendTransform(fake_scan_transform);
+    // tf::StampedTransform fake_scan_mount_transform, fake_scan_transform;
+    // fake_scan_mount_transform.frame_id_ = fake_scan_frame_id;
+    // fake_scan_mount_transform.child_frame_id_ = "fake_scan_mount_link";
+    // fake_scan_mount_transform.setOrigin((front_scan_mount_transform.getOrigin() + rear_scan_mount_transform.getOrigin()) / 2.0);
+    // fake_scan_mount_transform.setRotation(tf::Quaternion::getIdentity());
+    // fake_scan_mount_broadcaster.sendTransform(fake_scan_mount_transform);
+    //
+    // fake_scan_transform.frame_id_ = "fake_scan_mount_link";
+    // fake_scan_transform.child_frame_id_ = "fake_scan_link";
+    // fake_scan_transform.setOrigin((front_scan_transform.getOrigin() + rear_scan_transform.getOrigin()) / 2.0);
+    // fake_scan_transform.setRotation(tf::Quaternion::getIdentity());
+    // fake_scan_broadcaster.sendTransform(fake_scan_transform);
 
 
-        fake_scan_mount_transform.header.frame_id = fake_scan_mount_frame_id_;
-        fake_scan_mount_transform.child_frame_id = "fake_scan_mount_link";
+    fake_scan_mount_transform_.header.frame_id = fake_scan_mount_frame_id_;
+    fake_scan_mount_transform_.child_frame_id = "fake_scan_mount_link";
 
-        tf::vector3TFToMsg(front_scan_mount_transform_.getOrigin(), front_scan_mount_transform_origin);
-        tf::vector3TFToMsg(rear_scan_mount_transform_.getOrigin(), rear_scan_mount_transform_origin);
-        fake_scan_mount_transform_origin.x = (front_scan_mount_transform_origin.x + rear_scan_mount_transform_origin.x) / 2.0;
-        fake_scan_mount_transform_origin.y = (front_scan_mount_transform_origin.y + rear_scan_mount_transform_origin.y) / 2.0;
-        fake_scan_mount_transform_origin.z = (front_scan_mount_transform_origin.z + rear_scan_mount_transform_origin.z) / 2.0;
-        fake_scan_mount_transform.transform.translation = fake_scan_mount_transform_origin;
-        fake_scan_mount_transform_rotation.w = 1.0;
-        fake_scan_mount_transform_rotation.x = 0.0;
-        fake_scan_mount_transform_rotation.y = 0.0;
-        fake_scan_mount_transform_rotation.z = 0.0;
-        fake_scan_mount_transform.transform.rotation = fake_scan_mount_transform_rotation;
-        fake_scan_mount_broadcaster_.sendTransform(fake_scan_mount_transform);
+    tf::vector3TFToMsg(front_scan_mount_transform_.getOrigin(), front_scan_mount_transform_origin_);
+    tf::vector3TFToMsg(rear_scan_mount_transform_.getOrigin(), rear_scan_mount_transform_origin_);
+    fake_scan_mount_transform_origin_.x = (front_scan_mount_transform_origin_.x + rear_scan_mount_transform_origin_.x) / 2.0;
+    fake_scan_mount_transform_origin_.y = (front_scan_mount_transform_origin_.y + rear_scan_mount_transform_origin_.y) / 2.0;
+    fake_scan_mount_transform_origin_.z = (front_scan_mount_transform_origin_.z + rear_scan_mount_transform_origin_.z) / 2.0;
+    fake_scan_mount_transform_.transform.translation = fake_scan_mount_transform_origin_;
+    fake_scan_mount_transform_rotation_.w = 1.0;
+    fake_scan_mount_transform_rotation_.x = 0.0;
+    fake_scan_mount_transform_rotation_.y = 0.0;
+    fake_scan_mount_transform_rotation_.z = 0.0;
+    fake_scan_mount_transform_.transform.rotation = fake_scan_mount_transform_rotation_;
+    // fake_scan_mount_broadcaster_.sendTransform(fake_scan_mount_transform_);
 
-        fake_scan_transform.header.frame_id = "fake_scan_mount_link";
-        fake_scan_transform.child_frame_id = "fake_scan_link";
-        tf::vector3TFToMsg(front_scan_transform_.getOrigin(), front_scan_transform_origin);
-        tf::vector3TFToMsg(rear_scan_transform_.getOrigin(), rear_scan_transform_origin);
-        fake_scan_transform_origin.x = (front_scan_transform_origin.x + rear_scan_transform_origin.x) / 2.0;
-        fake_scan_transform_origin.y = (front_scan_transform_origin.y + rear_scan_transform_origin.y) / 2.0;
-        fake_scan_transform_origin.z = (front_scan_transform_origin.z + rear_scan_transform_origin.z) / 2.0;
-        fake_scan_transform.transform.translation = fake_scan_transform_origin;
-        fake_scan_transform_rotation.w = 1.0;
-        fake_scan_transform_rotation.x = 0.0;
-        fake_scan_transform_rotation.y = 0.0;
-        fake_scan_transform_rotation.z = 0.0;
-        fake_scan_transform.transform.rotation = fake_scan_transform_rotation;
-        fake_scan_broadcaster_.sendTransform(fake_scan_transform);
+    fake_scan_transform_.header.frame_id = "fake_scan_mount_link";
+    fake_scan_transform_.child_frame_id = "fake_scan_link";
+    tf::vector3TFToMsg(front_scan_transform_.getOrigin(), front_scan_transform_origin_);
+    tf::vector3TFToMsg(rear_scan_transform_.getOrigin(), rear_scan_transform_origin_);
+    fake_scan_transform_origin_.x = (front_scan_transform_origin_.x + rear_scan_transform_origin_.x) / 2.0;
+    fake_scan_transform_origin_.y = (front_scan_transform_origin_.y + rear_scan_transform_origin_.y) / 2.0;
+    fake_scan_transform_origin_.z = (front_scan_transform_origin_.z + rear_scan_transform_origin_.z) / 2.0;
+    fake_scan_transform_.transform.translation = fake_scan_transform_origin_;
+    fake_scan_transform_rotation_.w = 1.0;
+    fake_scan_transform_rotation_.x = 0.0;
+    fake_scan_transform_rotation_.y = 0.0;
+    fake_scan_transform_rotation_.z = 0.0;
+    fake_scan_transform_.transform.rotation = fake_scan_transform_rotation_;
+    // fake_scan_broadcaster_.sendTransform(fake_scan_transform_);
 
-        return;
+    return;
 }
 
-void HuskyPandaMobileScanMerger::convert_scan_to_cloud() {
-
+void HuskyPandaMobileScanMerger::set_transform() {
+    fake_scan_mount_broadcaster_.sendTransform(fake_scan_mount_transform_);
+    fake_scan_broadcaster_.sendTransform(fake_scan_transform_);
 }
 
 void HuskyPandaMobileScanMerger::merge_clouds() {
+    pcl::PointCloud<pcl::PointXYZ> transformed_pcl;
+    tf::TransformListener front_to_fake_listener;
+    tf::StampedTransform front_to_fake_transform;
+    try
+    {
+        // Verify that TF knows how to transform from the received scan to the destination scan frame
+        front_to_fake_listener.waitForTransform(front_scan_link_.c_str(), "fake_scan_link", ros::Time::now(), ros::Duration(1));
 
+        front_to_fake_listener.lookupTransform(front_scan_link_.c_str(), "fake_scan_link",
+                                                   ros::Time::now(), front_to_fake_transform);
+    }
+    catch (tf::TransformException ex)
+    {
+        ROS_ERROR("%s", ex.what());
+        ros::Duration(1.0).sleep();
+    }
+    Eigen::Vector3d front_to_fake_translation;
+    Eigen::Matrix3d front_to_fake_rotation;
+    Eigen::Quaterniond front_to_fake_quaternion;
+    tf::vectorTFToEigen(front_to_fake_transform.getOrigin(), front_to_fake_translation);
+    tf::quaternionTFToEigen(front_to_fake_transform.getRotation(), front_to_fake_quaternion);
+    front_to_fake_rotation = front_to_fake_quaternion.toRotationMatrix();
+    Eigen::Matrix4d front_to_fake_matrix;
+    front_to_fake_matrix.block<3, 3>(0, 0) = front_to_fake_rotation;
+    front_to_fake_matrix.block<3, 1>(0, 3) = front_to_fake_translation;
+    front_to_fake_matrix(3, 3) = 1.0;
+    pcl::transformPointCloud(front_pcl_cloud_, transformed_pcl, front_to_fake_matrix, true);
+
+    sensor_msgs::PointCloud2 temp_cloud;
+    pcl::toROSMsg(transformed_pcl, temp_cloud);
+    rear_cloud_publisher_.publish(temp_cloud);
 }
 
 void HuskyPandaMobileScanMerger::convert_cloud_to_scan() {
@@ -219,7 +276,6 @@ void HuskyPandaMobileScanMerger::convert_cloud_to_scan() {
     //     scan.time_increment = std::min(front_scan.time_increment, rear_scan.time_increment);
     //     laser_scan_publisher.publish(scan);
 }
-
 
 HuskyPandaMobileScanMerger::HuskyPandaMobileScanMerger(/* args */)
 {
@@ -246,6 +302,9 @@ HuskyPandaMobileScanMerger::HuskyPandaMobileScanMerger(/* args */)
     laser_scan_publisher_ = nh_.advertise<sensor_msgs::LaserScan>(fake_scan_topic_, 1000);
     front_cloud_publisher_ = nh_.advertise<sensor_msgs::PointCloud2> (front_cloud_topic_, 100, false);
     rear_cloud_publisher_ = nh_.advertise<sensor_msgs::PointCloud2> (rear_cloud_topic_, 100, false);
+
+    // listen for the tf transform
+    get_transform();
 
     // Start merging
     scan_merge();
