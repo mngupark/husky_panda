02869eedafd2087cd2ef1b2a055c07f7c50b2fc4
Gyuhub <Gyuhub@github.com>
2022-05-19 16:38:07 +0900

    add path following node

--- husky_panda_slam_toolbox/src/husky_panda_scan.cpp
+++ husky_panda_slam_toolbox/src/husky_panda_scan.cpp
@@ -232,7 +232,7 @@ HuskyPandaMobileScanMerger::~HuskyPandaMobileScanMerger()
 int main(int argc, char **argv)
 {
   // initialize
-  ros::init(argc, argv, "husky_panda_mobile_scan_node");
+  ros::init(argc, argv, "husky_panda_scan_node");
   // HuskyPandaMobileScanMerger husky_panda_mobile_scan_merger;
   auto husky_panda_mobile_scan_merger = boost::make_shared<HuskyPandaMobileScanMerger>();
   return 0;
