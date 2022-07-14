64bf108c221725ba103bad8c1780c5e0303fdafe
Gyuhub <alsrb0820@gmail.com>
2022-07-14 14:24:11 +0900

    modify an initial configuration of map environment file

--- husky_panda_slam/src/husky_panda_map_conversion.cpp
+++ husky_panda_slam/src/husky_panda_map_conversion.cpp
@@ -23,17 +23,20 @@ void HuksyPandaMapConverter::map_callback(const nav_msgs::OccupancyGrid::ConstPt
 
 void HuksyPandaMapConverter::initialize_map_configure(const char * file_name)
 {
+    // ROS_INFO("file name : %s", file_name);
+    // map_fd_ = open(file_name, O_CREAT | O_RDWR | O_TRUNC, 0644);
+    // char init_configuration[] = "discretization(cells): 119 119\n"
+    // "cellsize(meters): 0.025\n"
+    // "nominalvel(mpersecs): 0.4\n"
+    // "timetoturn45degsinplace(secs): 5.0\n"
+    // "start(meters,rads): 1.5 2.0 0 0\n"
+    // "end(meters,rads): 1.7 1.7 0 1\n"
+    // "environment:\n";
+    // write(map_fd_, init_configuration, strlen(init_configuration));
+    // close(map_fd_);
+
     ROS_INFO("file name : %s", file_name);
-    map_fd_ = open(file_name, O_CREAT | O_RDWR | O_TRUNC, 0644);
-    char init_configuration[] = "discretization(cells): 119 119\n"
-    "cellsize(meters): 0.025\n"
-    "nominalvel(mpersecs): 0.4\n"
-    "timetoturn45degsinplace(secs): 5.0\n"
-    "start(meters,rads): 1.5 2.0 0 0\n"
-    "end(meters,rads): 1.7 1.7 0 1\n"
-    "environment:\n";
-    write(map_fd_, init_configuration, strlen(init_configuration));
-    close(map_fd_);
+    map_fd_ = creat(file_name, 0644);
     return;
 }
 
@@ -43,19 +46,34 @@ void HuksyPandaMapConverter::generate_map(const nav_msgs::OccupancyGrid::ConstPt
     std::cout << "width: " << map->info.width << "\theight: " << map->info.height << '\n';
     std::cout << "origin: " << map->info.origin.position.x << ", " << map->info.origin.position.y << ", " << map->info.origin.position.z << '\n';
     std::cout << "resolution: " << map->info.resolution << '\n';
-    char buf[map->info.width * map->info.height];
-    char buf_ptr[256];
+
+    char info_buf[256];
+    sprintf(info_buf, 
+        "discretization(cells): %d %d\n"
+        "cellsize(meters): %f\n"
+        "nominalvel(mpersecs): 0.4\n"
+        "timetoturn45degsinplace(secs): 5.0\n"
+        "start(meters,rads): %f %f %f 0\n"
+        "end(meters,rads): 1.7 1.7 0 1\n"
+        "environment:\n",
+        map->info.width, map->info.height,
+        map->info.resolution,
+        map->info.origin.position.x, map->info.origin.position.y, map->info.origin.position.z
+    );
+    write(map_fd_, info_buf, strlen(info_buf));
+
+    char buf[256];
     for (auto i = 0; i < map->info.width; i++)
     {
         for (auto j = 0; j < map->info.height; j++)
         {
 
             int num = (unsigned char)map->data[i * map->info.width + j];
-            sprintf(buf_ptr, "%3d ", num);
-            write(map_fd_, buf_ptr, strlen(buf_ptr));
+            sprintf(buf, "%3d ", num);
+            write(map_fd_, buf, strlen(buf));
         }
-        strcpy(buf_ptr, "\n");
-        write(map_fd_, buf_ptr, strlen(buf_ptr));
+        strcpy(buf, "\n");
+        write(map_fd_, buf, strlen(buf));
     }
     close(map_fd_);
     ros::shutdown(); // create an environment file only once
